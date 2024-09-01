4a114139bf4aa6c2f97f9ea301b74f5c59b726c1
Peter Tripp <petertripp@gmail.com>
2024-09-01 13:44:29 -0400

    Working. Fetch with `/rfc 3339`

--- src/rfc.rs
+++ src/rfc.rs
@@ -36,14 +36,15 @@ impl zed::Extension for SlashCommandRfcExtension {
         } else if args.is_empty() {
             return Err("need rfc number".to_string());
         }
-        let Ok(rfc_number) = args[0].parse::<u32>() else {
-            return Err("invalid rfc number".to_string());
+        let rfc_number = match args[0].parse::<usize>() {
+            Ok(num) => num,
+            Err(_) => return Err("invalid rfc number".to_string()),
         };
 
         let rfc_zero = format!("{:04}", rfc_number);
-        let json_url = format!("{RFC_BASE_URL}/rfc{rfc_zero}.json");
+        let json_url = format!("{RFC_BASE_URL}/rfc{rfc_number}.json");
         let text_url = format!("{RFC_BASE_URL}/rfc{rfc_zero}.txt");
-        // Prepare the request
+
         let json_request = HttpRequest {
             method: HttpMethod::Get,
             url: json_url,
@@ -58,23 +59,29 @@ impl zed::Extension for SlashCommandRfcExtension {
             body: None,
             redirect_policy: RedirectPolicy::FollowAll,
         };
-        let rfc: Rfc = match zed::http_client::fetch(&json_request) {
-            Ok(response) => serde_json::from_slice(&response.body).unwrap(),
-            Err(e) => {
-                return Err(format!("Error: {}", e));
-            }
+
+        let response_json = zed::http_client::fetch(&json_request);
+        let rfc: Rfc = match response_json {
+            Ok(response) => match serde_json::from_slice(&response.body) {
+                Ok(rfc) => rfc,
+                Err(e) => return Err(format!("Failed to deserialize response. Error: {}", e)),
+            },
+            Err(e) => return Err(format!("Failed to fetch: {}", e)),
         };
 
-        let rfc_text = match zed::http_client::fetch(&text_request) {
-            Ok(response) => String::from_utf8(response.body).unwrap(),
-            Err(e) => {
-                return Err(format!("Error: {}", e));
-            }
+        let response_text = zed::http_client::fetch(&text_request);
+        let rfc_text = match response_text {
+            Ok(response) => match String::from_utf8(response.body) {
+                Ok(text) => text,
+                Err(e) => return Err(format!("Failed parsing UTF8 response. Error: {}", e)),
+            },
+            Err(e) => return Err(format!("Failed to fetch: {}", e)),
         };
         let clean_regex = Regex::new(r"\n.+\[Page \d+\][\n\u{c}]+(RFC \d+.+\d{4}\n\n)?").unwrap();
         let newline_regex = Regex::new("\n\n\n+").unwrap();
         let no_footy = clean_regex.replace_all(&rfc_text, "");
         let clean_text = newline_regex.replace_all(&no_footy, "\n\n");
+
         let text = clean_text.to_string();
         let sections = vec![SlashCommandOutputSection {
             range: Range {
@@ -106,7 +113,7 @@ where
 
 impl std::fmt::Display for Rfc {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{} {} ({})", self.doc_id, self.title, self.pub_date)
+        write!(f, "IETF {} {} ({})", self.doc_id, self.title, self.pub_date)
     }
 }
 
